91fe16d793424978e5ef601f950af8cd0089f0cf
DavidZ <davidbill@outlook.com>
2021-12-13 21:12:29 +0800

    :sparkles: canvas

--- manual/helper/src/renderer/components/ImageCanvas.vue
+++ manual/helper/src/renderer/components/ImageCanvas.vue
@@ -4,17 +4,267 @@
         class="h-[55vh]"
         :src="imgLocalUrl"
         alt="page"
+        @load="handleImgLoad"
     >
-    <canvas ref="canvas" class="absolute inset-0"></canvas>
+    <canvas
+        class="absolute top-0 h-[55vh]"
+        :style="`cursor.value: ${cursor.value}`"
+        ref="canvasRef"
+        @mousemove="handleMousemove"
+        @mouseout="handleMouseupAndMouseout"
+        @mousedown="handleMousedown"
+        @mouseup="handleMouseupAndMouseout"
+        @mouseenter="handleMouseenter"
+    ></canvas>
   </div>
 </template>
 
 <script setup>
-import { toRefs } from 'vue'
+import { ref, toRefs, watch } from 'vue'
+import { ObjectAnnotation } from '~/libs/annotationlib.js'
 
+const emits = defineEmits(['localAnnotationListUpdate'])
 const props = defineProps({
-  imgLocalUrl: String
+  imgLocalUrl: String,
+  currentPage: Number,
+  annotationList: Object
 })
-const { imgLocalUrl } = toRefs(props)
+const { imgLocalUrl, currentPage, annotationList } = toRefs(props)
+const canvasRef = ref()
 
+const localAnnotationList = ref([...annotationList.value])
+
+let imageHeight
+let imageWidth
+
+const draw = () => {
+  const ctx = canvasRef.value.getContext('2d')
+  ctx.clearRect(0, 0, imageWidth, imageHeight)
+  for (let annotation of localAnnotationList.value) {
+    if (annotation.page === currentPage.value) {
+      annotation.draw(ctx)
+    }
+  }
+}
+
+const handleImgLoad = (e) => {
+  const { naturalHeight: height, naturalWidth: width } = e.target
+  imageHeight = height
+  imageWidth = width
+  const canvasElement = canvasRef.value
+  canvasElement.height = height
+  canvasElement.width = width
+  draw()
+  emits('localAnnotationListUpdate', localAnnotationList.value)
+}
+
+watch(() => localAnnotationList.value, (newAnnotationList) => {
+  draw()
+  emits('localAnnotationListUpdate', newAnnotationList)
+}, { deep: true })
+
+watch(() => currentPage.value, () => draw())
+
+let createContext
+let activeContext
+let dragContext
+const cursor = ref('auto')
+const getMouseLocation = (event) => {
+  const canvasElement = canvasRef.value
+  const mouseX = event.offsetX / canvasElement.clientWidth * imageWidth
+  const mouseY = event.offsetY / canvasElement.clientHeight * imageHeight
+  return [mouseX, mouseY]
+}
+const handleMousemove = (event) => {
+  event.preventDefault()
+  const [mouseX, mouseY] = getMouseLocation(event)
+  // creating an object
+  if (createContext) {
+    const activeAnnotation = localAnnotationList.value[createContext.index]
+    const deltaX = mouseX - createContext.mousedownX
+    const deltaY = mouseY - createContext.mousedownY
+    activeAnnotation.resize(
+        createContext.x,
+        createContext.y,
+        deltaX,
+        deltaY
+    )
+  }
+  // drag the object
+  if (dragContext) {
+    const activeAnnotation = localAnnotationList.value[dragContext.index]
+    const deltaX = mouseX - dragContext.mousedownX
+    const deltaY = mouseY - dragContext.mousedownY
+    if (dragContext.type === 'moving') {
+      activeAnnotation.resize(
+          dragContext.x + deltaX,
+          dragContext.y + deltaY
+      )
+    } else if (dragContext.type === 'cornerSizing') {
+      const oppositeAnchor = dragContext.oppositeAnchor
+      activeAnnotation.resize(
+          oppositeAnchor.x,
+          oppositeAnchor.y,
+          (oppositeAnchor.x > dragContext.x ? -dragContext.width : dragContext.width) + deltaX,
+          (oppositeAnchor.y > dragContext.y ? -dragContext.height : dragContext.height) + deltaY
+      )
+    } else if (dragContext.type === 'topSizing') {
+      activeAnnotation.resize(
+          undefined,
+          mouseY,
+          undefined,
+          dragContext.height - deltaY
+      )
+    } else if (dragContext.type === 'bottomSizing') {
+      activeAnnotation.resize(
+          undefined,
+          mouseY > dragContext.y ? dragContext.y : mouseY,
+          undefined,
+          mouseY > dragContext.y ? dragContext.height + deltaY : dragContext.y - mouseY
+      )
+    } else if (dragContext.type === 'leftSizing') {
+      activeAnnotation.resize(
+          mouseX,
+          undefined,
+          dragContext.width - deltaX
+      )
+    } else if (dragContext.type === 'rightSizing') {
+      activeAnnotation.resize(
+          mouseX > dragContext.x ? dragContext.x : mouseX,
+          undefined,
+          mouseX > dragContext.x ? dragContext.width + deltaX : dragContext.x - mouseX
+      )
+    }
+  }
+  // highlight the object
+  let found = null
+  for (let i = 0; i < localAnnotationList.value.length; i++) {
+    const objectAnnotation = localAnnotationList.value[i]
+    if (!found && objectAnnotation.nearTopLeftAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'nw-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearTopAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'n-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearTopRightAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'ne-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearLeftAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'w-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearRightAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'e-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearBottomLeftAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'sw-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearBottomAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 's-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearBottomRightAnchor(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'se-resize'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else if (!found && objectAnnotation.nearBoundary(mouseX, mouseY)) {
+      if (!dragContext) cursor.value = 'grab'
+      objectAnnotation.heightighlight = true
+      found = i
+    } else {
+      objectAnnotation.heightighlight = false
+    }
+  }
+  if (typeof (found) === 'number') {
+    activeContext = {
+      index: found
+    }
+  } else {
+    activeContext = null
+    cursor.value = 'crosshair'
+  }
+}
+const handleMousedown = (event) => {
+  const [mouseX, mouseY] = getMouseLocation(event)
+  // drag
+  let found = false
+  for (let i = 0; i < localAnnotationList.value.length; i++) {
+    let objectAnnotation = localAnnotationList.value[i]
+    if (objectAnnotation.page === currentPage.value) {
+      if (objectAnnotation.heightighlight) {
+        found = true
+        let type = 'moving'
+        if (objectAnnotation.nearTopLeftAnchor(mouseX, mouseY) ||
+            objectAnnotation.nearTopRightAnchor(mouseX, mouseY) ||
+            objectAnnotation.nearBottomLeftAnchor(mouseX, mouseY) ||
+            objectAnnotation.nearBottomRightAnchor(mouseX, mouseY)) {
+          type = 'cornerSizing'
+        } else if (objectAnnotation.nearTopAnchor(mouseX, mouseY)) {
+          type = 'topSizing'
+        } else if (objectAnnotation.nearBottomAnchor(mouseX, mouseY)) {
+          type = 'bottomSizing'
+        } else if (objectAnnotation.nearLeftAnchor(mouseX, mouseY)) {
+          type = 'leftSizing'
+        } else if (objectAnnotation.nearRightAnchor(mouseX, mouseY)) {
+          type = 'rightSizing'
+        }
+        dragContext = {
+          index: i,
+          type: type,
+          x: objectAnnotation.x,
+          y: objectAnnotation.y,
+          width: objectAnnotation.weight,
+          height: objectAnnotation.height,
+          mousedownX: mouseX,
+          mousedownY: mouseY,
+          oppositeAnchor: type === 'cornerSizing' ? objectAnnotation.oppositeAnchor(mouseX, mouseY) : null
+        }
+        break
+      }
+    }
+  }
+  // creating an object
+  if (!found && !createContext) {
+    const objectAnnotation = new ObjectAnnotation(
+        mouseX, mouseY, 0, 0,
+        currentPage.value,
+        localAnnotationList.value.length
+    )
+    createContext = {
+      index: localAnnotationList.value.length,
+      x: objectAnnotation.x,
+      y: objectAnnotation.y,
+      mousedownX: mouseX,
+      mousedownY: mouseY
+    }
+    localAnnotationList.value.push(objectAnnotation)
+  }
+}
+const handleMouseupAndMouseout = (event) => {
+  event.preventDefault()
+  if (createContext) {
+    const activeAnnotation = localAnnotationList.value[createContext.index]
+    if (activeAnnotation.width < 8 || activeAnnotation.height < 8) {
+      localAnnotationList.value.splice(createContext.index, 1)
+    } else {
+      createContext = null
+    }
+    createContext = null
+  }
+  if (dragContext && event.type === 'mouseup') {
+    dragContext = null
+  }
+}
+const handleMouseenter = (event) => {
+  event.preventDefault()
+  // if left button of mouse is not pressed when entering the canvas, drag stops
+  if (event.buttons !== 1 && dragContext) {
+    dragContext = null
+  }
+}
 </script>
--- manual/helper/src/renderer/components/Instruction.vue
+++ manual/helper/src/renderer/components/Instruction.vue
@@ -9,28 +9,123 @@
         v-for="(manual, index) in item.manualList"
     >
   </div>
-  <div class="flex items-center justify-center gap-x-4 pt-6">
-    <button
-        @click="handlePrev"
-    ><ChevronLeftIcon class="w-6"/>
-    </button>
-    <ImageCanvas
-        v-if="item.manualList[currentManual].pageList[currentPage].localUrl"
-        :imgLocalUrl="item.manualList[currentManual].pageList[currentPage].localUrl"
-    ></ImageCanvas>
-    <button
-        @click="handleNext"
-    ><ChevronRightIcon class="w-6"/>
-    </button>
+  <div class="flex py-1">
+    <div class="flex-1">
+      <div class="flex items-center justify-center gap-x-4 py-4">
+        <div>
+          <button
+              class="rounded-tr-none rounded-br-none"
+              @click="handleFirst"
+          >
+            <ChevronDoubleLeftIcon class="w-6"/>
+          </button>
+          <button
+              class="rounded-tl-none rounded-bl-none"
+              @click="handlePrev"
+          >
+            <ChevronLeftIcon class="w-6"/>
+          </button>
+        </div>
+        <ImageCanvas
+            v-if="item.manualList[currentManual].pageList[currentPage].localUrl"
+            :currentPage="currentPage"
+            :imgLocalUrl="item.manualList[currentManual].pageList[currentPage].localUrl"
+            :annotationList="localAnnotationList"
+            @localAnnotationListUpdate="handleAnnotationListUpdate"
+        ></ImageCanvas>
+        <div>
+          <button
+              class="rounded-tr-none rounded-br-none"
+              @click="handleNext"
+          >
+            <ChevronRightIcon class="w-6"/>
+          </button>
+          <button
+              class="rounded-tl-none rounded-bl-none"
+              @click="handleLast"
+          >
+            <ChevronDoubleRightIcon class="w-6"/>
+          </button>
+        </div>
+      </div>
+      <div class="text-center">{{ currentPage + 1 }} / {{ item.manualList[currentManual].pageList.length }}</div>
+    </div>
+    <div class="flex-1 py-4">
+      <table class="w-full">
+        <thead>
+        <tr>
+          <th colspan="7">Annotation</th>
+        </tr>
+        <tr>
+          <th>step</th>
+          <th>page</th>
+          <th>x</th>
+          <th>y</th>
+          <th>height</th>
+          <th>width</th>
+          <th>operation</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(annotation, index) in localAnnotationList">
+          <td>{{ annotation.step }}</td>
+          <td>{{ annotation.page + 1 }}</td>
+          <td>{{ utils.toFixed2(annotation.x) }}</td>
+          <td>{{ utils.toFixed2(annotation.y) }}</td>
+          <td>{{ utils.toFixed2(annotation.height) }}</td>
+          <td>{{ utils.toFixed2(annotation.width) }}</td>
+          <td>
+            <button
+                class="rounded-tr-none rounded-br-none"
+                @click="currentPage = annotation.page"
+            >
+              <ZoomInIcon class="w-6"/>
+            </button>
+            <button
+                class="rounded-none"
+                @click="handleMoveUp(index)"
+            >
+              <ArrowUpIcon class="w-6"/>
+            </button>
+            <button
+                class="rounded-none"
+                @click="handleMoveDown(index)"
+            >
+              <ArrowDownIcon class="w-6"/>
+            </button>
+            <button
+                class="rounded-tl-none rounded-bl-none text-red-600"
+                @click="handleDelete(index)"
+            >
+              <TrashIcon class="w-6"/>
+            </button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-  <div class="text-center">{{ currentPage + 1 }} / {{ item.manualList[currentManual].pageList.length }}</div>
+  <div class="flex justify-center">
+    <button @click="handleSave">save</button>
+  </div>
 </template>
 
 <script setup>
 import { ref, toRefs, watch } from 'vue'
 import { getFileURL } from '~/utils/index.js'
-import {ChevronLeftIcon, ChevronRightIcon} from '@heroicons/vue/solid'
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  TrashIcon,
+  ZoomInIcon
+} from '@heroicons/vue/solid'
 import ImageCanvas from '~/components/ImageCanvas.vue'
+import utils from '~/libs/utils.js'
+import { ObjectAnnotation } from '~/libs/annotationlib.js'
 
 const props = defineProps({
   item: Object
@@ -40,7 +135,17 @@ const { item } = toRefs(props)
 const currentManual = ref(0)
 const currentPage = ref(0)
 
+watch(() => currentManual.value, () => {
+  currentPage.value = 0
+})
+
+const localAnnotationList = ref([])
+const handleAnnotationListUpdate = annotationList => {
+  localAnnotationList.value = annotationList
+}
+
 watch(() => item.value, (newItem) => {
+  localAnnotationList.value = []
   for (let i = 0; i < newItem.manualList.length; i++) {
     getFileURL(newItem.manualList[i].pathname).then(url => {
       newItem.manualList[i].localUrl = url
@@ -49,6 +154,11 @@ watch(() => item.value, (newItem) => {
       getFileURL(newItem.manualList[i].pageList[j].pathname).then(url => {
         newItem.manualList[i].pageList[j].localUrl = url
       })
+    }
+    for (let j in newItem.manualList[i].annotationList) {
+      const annotation = newItem.manualList[i].annotationList[j]
+      localAnnotationList.value.push(
+          new ObjectAnnotation(annotation.x, annotation.y, annotation.w, annotation.h, annotation.page, j))
     }
     currentManual.value = 0
     currentPage.value = 0
@@ -65,5 +175,30 @@ const handleNext = () => {
   if (currentPage.value !== item.value.manualList[currentManual.value].pageList.length - 1) {
     currentPage.value += 1
   }
+}
+
+const handleFirst = () => {
+  currentPage.value = 0
+}
+
+const handleLast = () => {
+  currentPage.value = item.value.manualList[currentManual.value].pageList.length - 1
+}
+
+const handleMoveUp = (index) => {
+  const annotationList = item.manualList[currentManual].annotationList
+
+}
+
+const handleMoveDown = (index) => {
+  const annotationList = item.manualList[currentManual].annotationList
+}
+
+const handleDelete = (index) => {
+  const annotationList = item.manualList[currentManual].annotationList
+}
+
+const handleSave = () => {
+
 }
 </script>
