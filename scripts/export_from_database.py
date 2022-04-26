5b4ca66035eab9a954e13736584730d95b9060af
DavidZ <davidbill@outlook.com>
2022-04-26 10:01:16 +1000

    :bug: remove unused manuals

--- scripts/export_from_database.py
+++ scripts/export_from_database.py
@@ -37,16 +37,33 @@ def get_item_list(raw_item_list):
                 and item['progressStatus'] \
                 and item['progressStatus'][0] \
                 and item['progressStatus'][1]:
+            manual_map = {}
             if 'manualList' in item \
                     and 'manualList' in ITEM_KEYS_TO_SAVE \
                     and MANUAL_KEYS_TO_SAVE:
                 for i in range(len(item['manualList'])):
-                    item['manualList'][i] = _copy_dict_by_keys(item['manualList'][i], MANUAL_KEYS_TO_SAVE)
+                    manual_map[i] = _copy_dict_by_keys(item['manualList'][i], MANUAL_KEYS_TO_SAVE)
+            manual_id_set = set()
             if 'annotationList' in item \
                     and 'annotationList' in ITEM_KEYS_TO_SAVE \
                     and ANNOTATION_KEYS_TO_SAVE:
                 for i in range(len(item['annotationList'])):
-                    item['annotationList'][i] = _copy_dict_by_keys(item['annotationList'][i], ANNOTATION_KEYS_TO_SAVE)
+                    manual_id_set.add(item['annotationList'][i]['manual'])
+                for i in range(len(item['manualList'])):
+                    if i not in manual_id_set:
+                        del manual_map[i]
+                new_manual_list = list(manual_map.values())
+                item['manualList'] = new_manual_list
+                manual_id_map = {}
+                for key, value in manual_map.items():
+                    for i in range(len(new_manual_list)):
+                        if new_manual_list[i]['url'] == value['url']:
+                            manual_id_map[key] = i
+                            break
+                for i in range(len(item['annotationList'])):
+                    new_annotation = _copy_dict_by_keys(item['annotationList'][i], ANNOTATION_KEYS_TO_SAVE)
+                    new_annotation['manual'] = manual_id_map[new_annotation['manual']]
+                    item['annotationList'][i] = new_annotation
             if 'videoList' in item \
                     and 'videoList' in ITEM_KEYS_TO_SAVE \
                     and VIDEO_KEYS_TO_SAVE:
